980d61c220db1432a32f5d964fc200110f78a5e0
zhoulipeng <zhoulpg@gmail.com>
2017-05-19 14:56:02 +0800

    guard 处理srs on_connect事件

--- guard/guard.go
+++ guard/guard.go
@@ -2,8 +2,10 @@ package main
 import (
     "io"
     "fmt"
+    "strings"
     "time"
     "net/http"
+    "io/ioutil"
     "log"
 )
 import "go-simplejson"
@@ -16,6 +18,31 @@ type GuardKey struct {
 func HelloServer(w http.ResponseWriter, req *http.Request) {
     
     //guard := &GuardKey{"PrivateKey123abc", 5600 + time.Now().Unix()}
+    req.ParseForm() //解析参数，默认是不会解析的 
+    //fmt.Fprintf(w, "Hi, I love you %s", html.EscapeString(req.URL.Path[1:]))
+    fmt.Println("method:", req.Method) //获取请求的方法 
+    if req.Method == "GET" {
+
+        fmt.Println("username", req.Form["username"]) 
+        fmt.Println("password", req.Form["password"]) 
+
+        for k, v := range req.Form {
+            fmt.Print("key:", k, "; ")
+            fmt.Println("val:", strings.Join(v, ""))
+        }
+        io.WriteString(w, "1")
+    } else if req.Method == "POST" {
+        result, _:= ioutil.ReadAll(req.Body)
+        req.Body.Close()
+        fmt.Printf("%s\n", result)
+        js, err := simplejson.NewJson(result);
+        ms := js.Get("tcUrl").MustString()
+        fmt.Println(ms)
+        fmt.Println(err)
+        io.WriteString(w, "0")
+        return
+    }
+    /*
     js, err := simplejson.NewJson(
         []byte(`{"action": "on_connect","client_id": 1985,
     "ip": "192.168.1.10", "vhost": "video.test.com", "app": "live",
@@ -24,13 +51,14 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
     ms := js.Get("tcUrl").MustString()
     fmt.Println(ms)
     fmt.Println(err)
-    io.WriteString(w, "PrivateKey123abc")
+    */
+    io.WriteString(w, "1")
 }
 func main() {
     fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
     fmt.Println("time is ", time.Now().Unix())
     http.HandleFunc("/api/guard", HelloServer)
-    err := http.ListenAndServe("127.0.0.1:12345", nil)
+    err := http.ListenAndServe(":12345", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
