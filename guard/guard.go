d70ec6fd8985ff9f8c3fe67afb1380945e18543d
zhoulipeng <zhoulpg@gmail.com>
2017-05-24 17:35:27 +0800

    use static safe_code , test Ok

--- guard/guard.go
+++ guard/guard.go
@@ -3,82 +3,106 @@ import (
     "io"
     "fmt"
     "strings"
+    "strconv"
     "time"
     "net/url"
     "net/http"
     "io/ioutil"
+    "crypto/md5" 
+    "encoding/hex"
     "log"
 )
 import "go-simplejson"
 
-type GuardKey struct {
-    key string
-    timeout int
+
+func hash_sum(raw string) string{
+    hasher := md5.New()
+    hasher.Write([]byte(raw))
+    return hex.EncodeToString(hasher.Sum(nil))
 }
-func authentication(w Request) {
+func auth_timestamp(time_str string) bool{
+    cur_gmt := time.Now().Unix()
+    fmt.Println("cur_gmt is:", cur_gmt);
+    req_gmt, err := strconv.ParseInt(time_str, 10, 64)
+
+    if err != nil {
+       fmt.Println("request's gmt time can't convert to int")
+       return false
+    }
+    fmt.Println("req_gmt is:", req_gmt);
+    if req_gmt < cur_gmt {
+        return false
+    }
+    return true
+}
+func auth_token(u *url.URL, m url.Values) bool{
+    safe_code := "##svi&&lgslb##"
+    hash_raw := u.Path +
+        "?user_id=" + strings.Join(m["user_id"], "") +
+        "&gmt="  + strings.Join(m["gmt"], "") +
+        "#" + safe_code 
+    fmt.Println(hash_raw)
+    hash_s := hash_sum(hash_raw)
+    hash_req := strings.Join(m["token"], "")
+    fmt.Println("request hash: ", hash_req)
+    fmt.Println("safe hash is: ", hash_s)
+    if(hash_req == hash_s){
+        fmt.Println("auth success.")
+        return true
+    }else{
+        fmt.Println("auth failed.")
+        return false
+    }
+    return true
+}
+func do_auth(result []byte) bool{
+    js, err := simplejson.NewJson(result);
+    if err != nil {
+        fmt.Println("bad json error: ", err) //raw url
+        return false
+    }
+    tc := js.Get("tcUrl").MustString()
+    fmt.Println(tc)
+        
+    //解析tcUrl
+    u, err := url.Parse(tc)
+    if err != nil {
+        fmt.Println(err)
+        return false
+    }
+    fmt.Println(u) //raw url
+    m, err := url.ParseQuery(u.RawQuery)
+    if err != nil {
+        fmt.Println(err)
+        return false
+    }
+    if !auth_timestamp(strings.Join(m["gmt"], "")) {
+        fmt.Println("auth timestamp failed")
+        return false
+    }
+    return auth_token(u, m)
+}
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-    
-    //guard := &GuardKey{"PrivateKey123abc", 5600 + time.Now().Unix()}
+
     req.ParseForm() //解析参数，默认是不会解析的 
-    //fmt.Fprintf(w, "Hi, I love you %s", html.EscapeString(req.URL.Path[1:]))
-    fmt.Println("method:", req.Method) //获取请求的方法 
-    if req.Method == "GET" {
-
-        fmt.Println("username", req.Form["username"]) 
-        fmt.Println("password", req.Form["password"]) 
-
-        for k, v := range req.Form {
-            fmt.Print("key:", k, "; ")
-            fmt.Println("val:", strings.Join(v, ""))
-        }
-    } else if req.Method == "POST" {
+    fmt.Println("method:", req.Method, "time is: ", time.Now())
+    if req.Method == "POST" {
         result, _:= ioutil.ReadAll(req.Body)
         req.Body.Close()
         fmt.Printf("%s\n", result)
-        js, err := simplejson.NewJson(result);
-        tc := js.Get("tcUrl").MustString()
-        fmt.Println(tc)
-        fmt.Println(err)
-        
-        //解析tcUrl
-        u, err := url.Parse(tc)
-        if err != nil {
-            log.Fatal(err)
+        if do_auth(result) {
+            io.WriteString(w, "0")
+            fmt.Println("method:", req.Method, "time is: ", time.Now())
+            return
         }
-        fmt.Println(u.Scheme)
-        fmt.Println(u.Opaque) // null
-        fmt.Println(u.Host)
-        fmt.Println(u.Path)
-        fmt.Println(u.RawPath) // null
-        fmt.Println(u.RawQuery) // ? params
-        fmt.Println(u.Fragment) // null
-        fmt.Println(u)
-        
-        u.Scheme = "https"
-        u.Host = "google.com"
-        q := u.Query()
-        q.Set("q", "golang")
-        u.RawQuery = q.Encode()
-        fmt.Println(u)
-        io.WriteString(w, "0")
-        return
+    } else {
+        fmt.Println("bad request method")
     }
-    /*
-    js, err := simplejson.NewJson(
-        []byte(`{"action": "on_connect","client_id": 1985,
-    "ip": "192.168.1.10", "vhost": "video.test.com", "app": "live",
-    "tcUrl": "rtmp://video.test.com/live?key=1444435200-0-0-80cd3862d699b7118eed99103f2a3a4f",
-    "pageUrl": "http://www.test.com/live.html"}`))
-    ms := js.Get("tcUrl").MustString()
-    fmt.Println(ms)
-    fmt.Println(err)
-    */
     io.WriteString(w, "1")
 }
 func main() {
     fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-    fmt.Println("time is ", time.Now().Unix())
     http.HandleFunc("/api/guard", HelloServer)
     err := http.ListenAndServe(":12345", nil)
     if err != nil {
