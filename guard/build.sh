7c10f3acea6b4a05a1514ed2f2a548920160d739
zhoulipeng <zhoulpg@gmail.com>
2017-06-04 23:34:44 +0800

    guard第三方库 url提交

--- guard/build.sh
+++ guard/build.sh
@@ -1,5 +1,10 @@
 # 首先安装依赖的json 库在 GOROOT目录下
 # cd /usr/lib/golang/src
 # git clone https://github.com/bitly/go-simplejson
+#mkdir -p src/github.com/bitly
+#midir -p src/github.com/takama
+#cd src/github.com/takama
+#git clone https://github.com/takama/daemon.git
+#cd ../../../
 export GOPATH="${PWD}"
 go build -o guard.exe
